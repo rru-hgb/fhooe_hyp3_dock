86024a8db2a4f4f583e95d9245a08a3da0476bce
Rimbert Rudisch-Sommer <rru-hgb@users.noreply.github.com>
2023-10-23 13:53:02 +0200

    initial import

---
+++ CleanReinstall.bat
@@ -0,0 +1,16 @@
+@echo off
+
+echo Stop all running containers
+docker compose down --volumes
+
+echo Remove all unused images, containers, networks and volumes
+docker system prune --volumes --all --force
+
+echo Update docker files from GitHub
+git pull
+
+echo Create and start the containers again in the background (detached)
+docker compose up --detach
+
+echo All finished. Enjoy your updated version!
+pause
